c8e35874131338ed6ebb71c62671ea70b2801793
MrJulEnergy <julian-peters@gmx.net>
2022-12-08 18:27:54 +0100

    general fixes (but doesnt work yet)

--- main.py
+++ main.py
@@ -1,59 +1,64 @@
-
 from zntrack import Node, zn
 import ase
 import ase_md.simulator as asemd
 import typing
 
-class MakeAtoms(Node):
+
+class GetAtoms(Node):
     """Generate Atoms for Simulation"""
-    #Inputs:
+
+    # Inputs:
     size: int = zn.params()
-    #Outputs:
+    # Outputs:
     atoms: ase.Atoms = zn.outs()
-    #Function:
+    # Function:
     def run(self):
         self.atoms = asemd.generate_atoms(self.size)
 
-if __name__ == "__main__":
-    cool_atoms = MakeAtoms(size=3)
-    cool_atoms.write_graph()
 
-'''
 class RunMD(Node):
     """Run Simulation"""
-    #Inputs:
+
+    # Inputs:
     atoms: ase.Atoms = zn.deps()
     temperature: float = zn.params()
     timestep: float = zn.params()
     dump_interval: int = zn.params()
     steps: int = zn.params()
-    #Outputs:
-    atoms_list: typing.List[ase.Atoms] = zn.outs()
-    #Functions:
-    def run_md(self):
-        self.atoms_list = asemd.run_simulation(
+    # Outputs:
+    atom_list: typing.List[ase.Atoms] = zn.outs()
+    # Functions:
+    def run(self):
+        self.atom_list = asemd.run_simulation(
             atoms=self.atoms,
             temperature=self.temperature,
             timestep=self.timestep,
-            dump_interval=self.dump_interval,
             steps=self.steps,
+            dump_interval=self.dump_interval,
         )
 
 
 class ComputeRDF(Node):
     """Calculate RDF whatever that means..."""
-    #Inputs:
+    # Inputs:
     atoms_list: typing.List[ase.Atoms] = zn.deps()
     rmax: float = zn.params()
     nbins: int = zn.params()
-    #Outputs:
-    rfd: dict = zn.metrics()
-    #Function:
-    def calc_rfd(self):
+    # Outputs:
+    rfd: dict = zn.outs()
+    # Function:
+    def run(self):
         self.rfd = asemd.compute_rdf(
-            atoms_list=self.atoms_list, 
-            rmax=self.rmas, 
-            nbins=self.nbins, 
-            elements="Cu"
+            atoms_list=self.atoms_list, rmax=self.rmas, nbins=self.nbins, elements="Cu"
         )
-'''
+
+if __name__ == "__main__":
+    cool_atoms = GetAtoms(size=3)
+    cool_atoms.write_graph()
+    cool_atoms_list = RunMD(
+        atoms=cool_atoms, temperature=300, timestep=5.0, steps=20, dump_interval=10
+    )
+    cool_atoms_list.write_graph()
+    res_rdf = ComputeRDF(atoms_list=cool_atoms_list, rmax=1.0, nbins=50)
+    res_rdf.write_graph()
+
